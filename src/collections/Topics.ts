823c18e007baf096740f5252562c89eaeba8f17b
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-12-07 08:51:32 -0800

    Issue pg-irc/pathways-remix#722 Create Topics collection

---
+++ src/collections/Topics.ts
@@ -0,0 +1,32 @@
+import type { CollectionConfig } from 'payload';
+
+export const Topics: CollectionConfig = {
+    slug: 'topic',
+    access: {
+        read: () => true,
+    },
+    fields: [
+        {
+            name: 'name',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'content',
+            type: 'textarea',
+            required: true,
+        },
+        {
+            name: 'heroimage',
+            type: 'upload',
+            relationTo: 'media',
+            required: false,
+        },
+        {
+            name: 'listImage',
+            type: 'upload',
+            relationTo: 'media',
+            required: false,
+        }
+    ],
+};
