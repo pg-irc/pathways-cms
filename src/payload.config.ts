fb39d664ec2010982415bea182da618f77054fe8
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-12-07 08:45:05 -0800

    Issue pg-irc/pathways-remix#722 Fixed white space

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -14,24 +14,24 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    importMap: {
-      baseDir: path.resolve(dirname),
+    admin: {
+        user: Users.slug,
+        importMap: {
+            baseDir: path.resolve(dirname),
+        },
     },
-  },
-  collections: [Users, Media],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
-  }),
-  sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
-  ],
+    collections: [Users, Media],
+    editor: lexicalEditor(),
+    secret: process.env.PAYLOAD_SECRET || '',
+    typescript: {
+        outputFile: path.resolve(dirname, 'payload-types.ts'),
+    },
+    db: mongooseAdapter({
+        url: process.env.DATABASE_URI || '',
+    }),
+    sharp,
+    plugins: [
+        payloadCloudPlugin(),
+        // storage-adapter-placeholder
+    ],
 })
