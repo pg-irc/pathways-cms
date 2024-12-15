c44655ad0a2df7023b414e81f2d41c67d86ac8cf
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-12-15 11:09:34 -0800

    Issue pg-irc/pathways-remix#722 Completed the script

--- src/bin/seedTopics2.ts
+++ src/bin/seedTopics2.ts
@@ -1,7 +1,21 @@
 import fs from 'fs';
 import readline from 'readline';
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { createHeadlessEditor } from '@payloadcms/richtext-lexical/lexical/headless';
+import { $convertFromMarkdownString, TRANSFORMERS } from '@lexical/markdown';
+import { LinkNode } from '@payloadcms/richtext-lexical';
+import { ListItemNode, ListNode } from '@payloadcms/richtext-lexical/lexical/list';
+import { HorizontalRuleNode } from '@payloadcms/richtext-lexical/lexical/react/LexicalHorizontalRuleNode';
+import { HeadingNode, QuoteNode } from '@payloadcms/richtext-lexical/lexical/rich-text';
+import { LineBreakNode, ParagraphNode, RootNode, TextNode } from 'lexical';
+
 
 const filename = '../content/topics/bc/markdown/Newcomers-Guide-English.md';
+const locale = 'en';
+const region = 'bc';
+
+let count = 0;
 
 const main =  async () => {
     await processFileLineByLine(filename)
@@ -9,26 +23,29 @@ const main =  async () => {
 };
 
 async function processFileLineByLine(filePath: string): Promise<void> {
-    const fileStream = fs.createReadStream(filePath);
-    const rl = readline.createInterface({
-        input: fileStream,
+    const theFileStream = fs.createReadStream(filePath);
+    const theLineReader = readline.createInterface({
+        input: theFileStream,
         crlfDelay: Infinity
     });
-    for await (const line of rl) {
+    for await (const line of theLineReader) {
         processLine(line);
     }
 }
 
 const processLine = (line: string) => {
     if (line.startsWith('CHAPTER//')) {
-        completeTopic();
-        chapter = getValue(line);
+        persistCurrentTopic();
+        chapter = getValueFromLine(line);
+
     } else if (line.startsWith('TOPIC//')) {
-        completeTopic();
-        localizedName = getValue(line);
+        persistCurrentTopic();
+        localizedName = getValueFromLine(line);
+
     } else if (line.startsWith('ID//')) {
-        completeTopic();
-        canonicalName = getValue(line);
+        persistCurrentTopic();
+        canonicalName = getValueFromLine(line);
+
     } else if (line.startsWith('MAPS_QUERY//') || line.startsWith('Tags')) {
         // do nothing
     } else {
@@ -36,21 +53,98 @@ const processLine = (line: string) => {
     }
 };
 
-const getValue = (line: string) => line.split('//')[1];
+const getValueFromLine = (line: string) => line.split('//')[1];
 
 let chapter = '';
 let localizedName = '';
 let canonicalName = '';
 let localizedContent = '';
 
-const completeTopic = () => {
+const persistCurrentTopic = () => {
     if (!localizedContent) {
         return;
     }
-    console.log('Chapter:', chapter);
-    console.log('LocalizedName:', localizedName);
-    console.log('CanonicalName:', canonicalName);
+    count++;
+    if (count > 4) {
+        console.log('Skipping ', canonicalName);
+        return;
+    }
+    const topicId = getTopicId();
+    setLocalizedContent(topicId);
     localizedContent = '';
+};
+
+const payload = await getPayload({ config })
+
+const getTopicId = () => {
+    const r1 = payload.find({ collection: 'topic', where: { canonicalName } });
+    if (r1.length > 0) { return r1[0].id; }
+
+    const chapterId = getChapterId(chapter);
+    const topicTypeId = '6758af0570f85c9507213d82';
+    const r2 = payload.create({
+        collection: 'topic',
+        data: {
+            canonicalName,
+            chapters: [chapterId],
+            topictype: topicTypeId,
+        },
+        locale,
+        fallbackLocale: locale,
+    });
+    return r2.id;
+};
+
+const getChapterId = async (name: string): string => {
+    const r1 = await payload.find({ collection: 'chapter', where: { name } });
+    if (r1.length > 0) { return r1[0].id; }
+
+    const r2 = await payload.create({ collection: 'chapter', data: { name } });
+    return r2.id;
+};
+
+const setLocalizedContent = async (topicId: string) => {
+    await payload.update({
+        collection: 'topic',
+        id: topicId,
+        data: {
+            localizedName: localizedName,
+            content: convertMarkdownToLexical(localizedContent),
+        },
+        locale,
+        fallbackLocale: 'en',
+    });
+};
+
+const convertMarkdownToLexical = (markdownString: string): string => {
+    const editor = createHeadlessEditor({
+        nodes: [
+            HeadingNode,
+            ListNode,
+            ListItemNode,
+            QuoteNode,
+            LineBreakNode,
+            ParagraphNode,
+            RootNode,
+            TextNode,
+            // CodeNode,
+            // CodeHighlightNode,
+            // TableNode,
+            // TableCellNode,
+            // TableRowNode,
+            // AutoLinkNode,
+            LinkNode,
+            HorizontalRuleNode
+        ],
+        onError: (error) => console.error(error),
+    });
+    
+    editor.update(() => {
+        $convertFromMarkdownString(markdownString, TRANSFORMERS);
+    }, {discrete: true});
+    
+    const result = editor.getEditorState().toJSON();
+    return JSON.stringify(result);
 };
 
 main();
